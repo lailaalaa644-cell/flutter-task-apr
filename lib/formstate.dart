fba1a61e933112823dc2a9714876564f9e7b7315
Laila Alaa <lailaalaa644@gmail.com>
2026-05-04 09:22:33 +0300

    .

--- lib/formstate.dart
+++ lib/formstate.dart
@@ -2,41 +2,21 @@ import 'package:equatable/equatable.dart';
 
 class Formstate extends Equatable {
   final String email;
-  final String? emailError;
-  final String password;
-  final String? passError;
-  final bool isSubmit;
+  final String emailError;
+  final String pass;
+  final String passerror;
+  final bool sbmit;
 
   Formstate({
-    this.email = "",
-    this.emailError,
-    this.password = "",
-    this.passError,
-    this.isSubmit = false,
+    required this.email,
+    required this.emailError,
+    required this.pass,
+    required this.passerror,
+    required this.sbmit,
   });
 
-  Formstate copyWith({
-    String? email,
-    String? emailError,
-    String? password,
-    String? passError,
-    bool? isSubmit,
-  }) {
-    return Formstate(
-      email: email ?? this.email,
-      emailError: emailError,
-      password: password ?? this.password,
-      passError: passError,
-      isSubmit: isSubmit ?? this.isSubmit,
-    );
-  }
-
-  @override
-  List<Object> get props => [
-    email,
-    emailError ?? "",
-    password,
-    passError ?? "",
-    isSubmit,
-  ];
 }
+  Formstate copyWith({
+    String? email;
+    String? emailError;
+  })
